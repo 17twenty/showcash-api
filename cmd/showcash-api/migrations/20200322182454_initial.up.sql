24a89d67a8cc23dc8713e66dd71f273c5b53f3c6
Nick Glynn <nick.glynn@versent.com.au>
2020-03-22 22:47:13 +1100

    Formatting SQL :

--- cmd/showcash-api/migrations/20200322182454_initial.up.sql
+++ cmd/showcash-api/migrations/20200322182454_initial.up.sql
@@ -12,7 +12,6 @@ $$ LANGUAGE plpgsql;
 
 CREATE TABLE IF NOT EXISTS showcash.post (
     user_id         UUID NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000', -- This is blank for v1
-    
     -- Extracted
     id              UUID PRIMARY KEY,
     title           TEXT NOT NULL DEFAULT '',
@@ -25,15 +24,15 @@ CREATE TABLE IF NOT EXISTS showcash.post (
 
 
 CREATE TABLE IF NOT EXISTS showcash.item (
-    post_id       UUID NOT NULL,
+    post_id         UUID NOT NULL,
     -- Extracted
-    id            BIGINT NOT NULL DEFAULT 0,
-    title         TEXT NOT NULL DEFAULT '',
-    description   TEXT NOT NULL DEFAULT '',
-    link          TEXT NOT NULL DEFAULT '',
-    "left" BIGINT NOT NULL DEFAULT 0,
-    top BIGINT NOT NULL DEFAULT 0,
+    id              BIGINT NOT NULL DEFAULT 0,
+    title           TEXT NOT NULL DEFAULT '',
+    description     TEXT NOT NULL DEFAULT '',
+    link            TEXT NOT NULL DEFAULT '',
+    "left"          BIGINT NOT NULL DEFAULT 0,
+    top             BIGINT NOT NULL DEFAULT 0,
     -- End Extracted
     PRIMARY KEY(post_id, id),
-    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
